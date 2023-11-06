49787e4af312fe11a1e0584f69af6136223af4a2
Lucas Leandro <lucasleandro.viana@gmail.com>
2023-11-06 17:06:55 -0300

    url do index arrumada nas rotas

--- src/rotas.jsx
+++ src/rotas.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Inspiracao  from "./components/pages/Inspiracao";
 import Contato from "./components/pages/Contato";
 import Produto from "./components/pages/Produto";
-import Index from "./components/pages/";
+import Index from "./components/pages/Index";
 
 
  function Rotas(){
